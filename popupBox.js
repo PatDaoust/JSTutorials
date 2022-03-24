7dc776b885d65d97943dce5ce1531fc1f263f466
Cataluna <catalunalilith2680@gmail.com>
2022-03-24 19:14:54 -0400

    implement exercise popupBox

---
+++ popupBox.js
@@ -0,0 +1,6 @@
+//Make a variable test set it equal to a prompt box
+//type "Hi!" in it (without the quotes) when it pops up
+//Note: your pop-up blocker must not be enabled.
+var test = prompt("Please type 'Hi!'");
+
+console.log(test);
