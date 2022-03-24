cf72dadd32a8e40c7efb144afba8450d98f2a7b5
Cataluna <catalunalilith2680@gmail.com>
2022-03-24 15:47:55 -0400

    implement ecxercise push shift

---
+++ arrayPushShift.js
@@ -0,0 +1,11 @@
+//manipulate the array called myArray and do the following:
+//Push the number 42 to the end of the array
+//Shift a variable from the beginning of the array
+
+var myArray = [true, "What is the meaning of life?"];
+// TODO: write your code here:
+myArray.push(42);
+myArray.shift();
+
+console.log(myArray[0]);
+console.log(myArray[1]);
