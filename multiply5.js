457e91dcc660caf1d1b59f341da37c9145fd7d94
Cataluna <catalunalilith2680@gmail.com>
2022-03-24 19:10:00 -0400

    implement exercise multiplyFive

---
+++ multiply5.js
@@ -0,0 +1,10 @@
+//Define a function called multiplyFive which accepts a number and returns that number multiplied by 5.
+
+function multiplyFive(num){
+    return num*5
+}
+
+console.log(multiplyFive(3));
+console.log(multiplyFive(4));
+console.log(multiplyFive(5));
+console.log(multiplyFive(6));
---
+++ times5.js
@@ -0,0 +1,2 @@
+//Define a function called multiplyFive which accepts a number and returns that number multiplied by 5.
+
