69605b74fa185999c54362ac79efbf9fc65b717e
Cataluna <catalunalilith2680@gmail.com>
2022-03-24 19:01:42 -0400

    implement exercise personObject

---
+++ personObject.js
@@ -0,0 +1,18 @@
+//define an object called person with the following members:
+//The member firstName of the person is "Jack"
+//The member lastName of the person is "Smith"
+//The member age of the person is 19
+//The member employed of the person is true
+
+// TODO: change this code
+var person = {
+    firstName: "Jack",
+    lastName: "Smith",
+    age: 19,
+    employed: true
+}
+
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
+console.log(person.employed);
