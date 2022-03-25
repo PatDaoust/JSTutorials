b45251d9aea49d2c5ee5997c01836ffd556df6e1
Cataluna <catalunalilith2680@gmail.com>
2022-03-25 12:40:09 -0400

    implement exercise divideByTwo

---
+++ divideByTwo.js
@@ -0,0 +1,8 @@
+//Define an arrow function divideByTwo which accepts a number and returns that number divided by 2.
+
+let divideByTwo = num => num/2
+
+console.log(divideByTwo(4));
+console.log(divideByTwo(8));
+console.log(divideByTwo(12));
+console.log(divideByTwo(6));
