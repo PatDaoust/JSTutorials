460c8c2423e75a7f8a86ab29c1c7650d60295385
Cataluna <catalunalilith2680@gmail.com>
2022-03-26 19:03:33 -0400

    implement exercise callBind

---
+++ callBind.js
@@ -0,0 +1,23 @@
+//Create bound copies of printFullName and printDetails to person called 
+//boundPrintFullName and boundPrintDetails
+var person = {
+    firstName : "John",
+    lastName : "Smith",
+    age : 23
+};
+
+function printFullName() {
+    console.log(this.firstName + " " + this.lastName);
+}
+
+function printDetails(){
+    console.log(this.firstName + " is " + this.age + " years old");
+}
+
+var boundPrintFullName = printFullName.bind(person);
+var boundPrintDetails = printDetails.bind(person);
+
+boundPrintFullName();
+boundPrintDetails();
+
+
